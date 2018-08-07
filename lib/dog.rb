08c2822f9aa1c54c33e3f1dfaf960cf2ce5ffc76
David Zeit <dzeit216@gmail.com>
2018-08-07 17:55:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@
 class Dog
   def initialize(name)
     @name = name
-  end 
+  end
 
   def name=(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
   def name
-    @name 
-  end 
-end 
+    @name
+  end
+end
